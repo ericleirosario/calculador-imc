ebddd34bd6522c6b56d9745624c3ab3ceace50f1
ericleirosario <ericleirosario@gmail.com>
2023-09-07 13:38:07 -0300

    Alteração nas condicionais em script.js

--- script.js
+++ script.js
@@ -6,21 +6,27 @@ function calculaImc() {
 
     const imc = (peso / (altura * altura)).toFixed(2);
 
-    if(imc > 0 && imc < 17) {
-        resultado.innerHTML = "\nVocê está muito abaixo do peso";	
-    } else if(imc >=17 && imc < 18.5){
-        resultado.innerHTML = "\nVocê está abaixo do peso";
-    } else if(imc >= 18.5 && imc < 25){
-        resultado.innerHTML = "\nSeu peso está normal";	
-    } else if(imc >= 25 && imc < 30){
-        resultado.innerHTML = "\nVocê está acima do peso";
-    } else if(imc >= 30 && imc < 35){
-        resultado.innerHTML = "Obesidade grau 1";
-    } else if(imc >= 35 && imc < 41){
-        resultado.innerHTML = "Obesidade grau 2";
-    } else if(peso == 0 && altura == 0) {
-        resultado.innerHTML = "Nem um valor digitado";
+    if(peso == 0 && altura == 0){
+        resultado.innerHTML = "Insira seu peso e sua altura!";
+    } else if(peso == 0){
+        resultado.innerHTML = "Insira seu peso!"
+    } else if(altura == 0){
+        resultado.innerHTML = "Insira sua altura!"
     } else {
-        resultado.innerHTML = "Obesidade grau 3";
+        if(imc > 0 && imc < 17) {
+            resultado.innerHTML = "Seu IMC é: " +imc+ " Você está muito abaixo do peso";	
+        } else if(imc >=17 && imc < 18.5){
+            resultado.innerHTML = "Seu IMC é: " +imc+ " Você está abaixo do peso";
+        } else if(imc >= 18.5 && imc < 25){
+            resultado.innerHTML = "Seu IMC é: " +imc+ " Seu peso está normal";	
+        } else if(imc >= 25 && imc < 30){
+            resultado.innerHTML = "Seu IMC é: " +imc+ " Você está acima do peso";
+        } else if(imc >= 30 && imc < 35){
+            resultado.innerHTML = "Seu IMC é: " +imc+ " Obesidade grau 1";
+        } else if(imc >= 35 && imc < 41){
+            resultado.innerHTML = "Seu IMC é: " +imc+ " Obesidade grau 2";
+        } else {
+            resultado.innerHTML = "Seu IMC é: " +imc+ " Obesidade grau 3";
+        }
     }
 }
